a874c7f9a8e0cc89af0b87488660a9dfa45ca61e
Vitor Azeredo <esquilopro0@gmail.com>
2025-09-02 14:40:34 -0300

    changes to the for loop

--- src/aulas/vitor/aprendendojava/introducao/ExercicioArrayMultidimensionais.java
+++ src/aulas/vitor/aprendendojava/introducao/ExercicioArrayMultidimensionais.java
@@ -9,21 +9,18 @@ public class ExercicioArrayMultidimensionais {
                 {5, 4, 7}    // Aluno 3
         };
 
-        for (int i = 0; i < notas.length; i++) {
-            System.out.print("Notas do Aluno " + (i + 1) + ": ");
-            for (int j = 0; j < notas[i].length; j++) {
-                System.out.print(notas[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        for (int i = 0; i < notas.length; i++) {
+        for (int numAluno = 0; numAluno < notas.length; numAluno++) {
             int soma = 0;
-            for (int j = 0; j < notas[i].length; j++) {
-                soma += notas[i][j]; // soma as notas do aluno
+            System.out.print("Notas do Aluno " + (numAluno + 1) + ": ");
+
+            for (int notasAlunos = 0; notasAlunos < notas[numAluno].length; notasAlunos++) {
+                int nota = notas[numAluno][notasAlunos];
+                soma += nota;
+                System.out.print(nota + " ");
             }
-            double media = (double) soma / notas[i].length;
-            System.out.printf("Média de notas do Aluno %d: %.2f%n", (i + 1), media);
+
+            double media = (double) soma / notas[numAluno].length;
+            System.out.printf("- Média: %.2f%n", media);
         }
     }
 }
