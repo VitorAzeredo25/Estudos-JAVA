06badb46c3032a9d28d1df4f7d24b29cbeeb5e7f
Vitor Azeredo <esquilopro0@gmail.com>
2025-09-05 09:54:49 -0300

    add files

---
+++ src/aulas/vitor/aprendendojava/objectoriented/dominio/Calculadora.java
@@ -0,0 +1,17 @@
+package aulas.vitor.aprendendojava.objectoriented.dominio;
+
+public class Calculadora {
+
+    public void somarDados(int num1, int num2){
+        System.out.println(num1 + num2);
+    }
+
+    public void subtrairDados(int num1, int num2){
+        System.out.println(num1 - num2);
+    }
+
+    public void multiplicarDados(int num1, int num2){
+        System.out.println(num1 * num2);
+    }
+
+}
---
+++ src/aulas/vitor/aprendendojava/objectoriented/test/TesteCalculadora.java
@@ -0,0 +1,13 @@
+package aulas.vitor.aprendendojava.objectoriented.test;
+
+import aulas.vitor.aprendendojava.objectoriented.dominio.Calculadora;
+
+public class TesteCalculadora {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+        calculadora.somarDados(10, 20);
+        calculadora.subtrairDados(100,59);
+        calculadora.multiplicarDados(7,7);
+    }
+}
